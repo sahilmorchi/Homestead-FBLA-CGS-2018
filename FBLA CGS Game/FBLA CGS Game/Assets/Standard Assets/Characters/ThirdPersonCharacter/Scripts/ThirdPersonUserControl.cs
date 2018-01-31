503a1d8f22f7266d38f635d034af9fb803f95fef
Yash Bengali <yashbfalcon@gmail.com>
2018-01-30 20:47:44 -0800

    Wrote character control script
    I wrote a scipt that uses controls we wanted instead of built in prefab controls

--- FBLA CGS Game/FBLA CGS Game/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ FBLA CGS Game/FBLA CGS Game/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -7,29 +7,93 @@ namespace UnityStandardAssets.Characters.ThirdPerson
     [RequireComponent(typeof (ThirdPersonCharacter))]
     public class ThirdPersonUserControl : MonoBehaviour
     {
-        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
-        private Vector3 m_CamForward;             // The current forward direction of the camera
-        private Vector3 m_Move;
-        private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+		public float inputDelay = 0.1f;
+		public float forwardVel = 12;
+		public float rotateVel = 100;
+
+		Quaternion targetRotation;
+		Rigidbody rbody;
+		float forwardInput, turnInput;
+
+		public Quaternion TargetRotation {
+			get{ return TargetRotation; }
+		}
+
+		void Start(){
+			targetRotation = transform.rotation;
+			if (GetComponent<Rigidbody> ())
+				rbody = GetComponent<Rigidbody> ();
+			else
+				Debug.LogError ("The character needs a rigidbody.");
+
+			forwardInput = turnInput = 0;
+		}
+
+		void GetInput()
+		{
+			forwardInput = Input.GetAxis ("Vertical");
+			turnInput = Input.GetAxis ("Horizontal");
+		}
+
+		void Update()
+		{
+			GetInput();
+			Turn();
+		}
+
+		void FixedUpdate()
+		{
+			Run();
+		}
+
+		void Run()
+		{
+			if (Mathf.Abs (forwardInput) > inputDelay) {
+				// move
+				rbody.velocity = transform.forward * forwardInput * forwardVel;
+			} 
+			else {
+				// zero velocity
+				rbody.velocity = Vector3.zero;
+			}
+
+		}
+
+		void Turn()
+		{
+			if (Mathf.Abs (turnInput) > inputDelay) {
+				// move
+				targetRotation *= Quaternion.AngleAxis (rotateVel * turnInput*Time.deltaTime, Vector3.up);
+			} 
+			transform.rotation = targetRotation;
+		}
+
+
+
+       /*
+		// private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+       // private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+        //private Vector3 m_CamForward;             // The current forward direction of the camera
+        //private Vector3 m_Move;
+        //private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 
         
         private void Start()
         {
             // get the transform of the main camera
-            if (Camera.main != null)
-            {
-                m_Cam = Camera.main.transform;
-            }
-            else
-            {
-                Debug.LogWarning(
-                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+         //   if (Camera.main != null)
+           // {
+            //    m_Cam = Camera.main.transform;
+            //}
+            //else
+            //{
+              //  Debug.LogWarning(
+                //    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
                 // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
             }
 
             // get the third person character ( this should never be null due to require component )
-            m_Character = GetComponent<ThirdPersonCharacter>();
+            //m_Character = GetComponent<ThirdPersonCharacter>();
         }
 
 
@@ -70,6 +134,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // pass all parameters to the character control script
             m_Character.Move(m_Move, crouch, m_Jump);
             m_Jump = false;
-        }
+        }*/
     }
 }
